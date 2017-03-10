0b752430799aa12807d22c7b1965c664458fcfd9
Ian Parker <ian@geekprojects.com>
2017-03-10 22:42:41 +0000

    Syscalls/System: Slightly better uname response

--- syscalls/system.cpp
+++ syscalls/system.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/utsname.h>
 
 #include "kernel.h"
 #include "system.h"
@@ -12,11 +13,13 @@ SYSCALL_METHOD(uname)
 #ifdef DEBUG
     log("execSyscall: sys_utsname: utsname=%p", utsname);
 #endif
+    struct utsname osx_utsname;
+    uname(&osx_utsname);
     strcpy(utsname->sysname, "Linux");
-    strcpy(utsname->nodename, "LinuxOnMac");
+    strcpy(utsname->nodename, osx_utsname.nodename);
     strcpy(utsname->release, "4.4.24");
-    strcpy(utsname->version, "4.4.24"); // The version I've been using as a reference
-    strcpy(utsname->machine, "x86_64");
+    strcpy(utsname->version, "line 4.4.24 (Linux on Mac)"); // The version I've been using as a reference
+    strcpy(utsname->machine, osx_utsname.machine);
     ucontext->uc_mcontext->__ss.__rax = 0;
     return true;
 }
